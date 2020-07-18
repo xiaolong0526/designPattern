c634a1ae5743b696c672d83d84f8eb1b8a3172e4
xiaolong <xlshi1996@163.com>
2020-07-18 12:35:36 +0800

    修改单例原则

--- src/com/xiaolong/principle/singleResponsibility/SingleResponsibilityDemo01.java
+++ src/com/xiaolong/principle/singleResponsibility/SingleResponsibilityDemo01.java
@@ -11,16 +11,17 @@ package com.xiaolong.principle.singleResponsibility;
  */
 public class SingleResponsibilityDemo01 {
     public static void main(String[] args) {
-        //这里就存在问题：一个类中实现了多个功能，需要把类细粒度化，分成天上，地上，水上
+        //这里就存在问题：一个类中实现了多个功能，违反了单一职责原则，
+        //解决方案：1.根据交通方式不同分成不同的类；2.根据交通方式不同分成不同的方法
         Vehilce vehilce = new Vehilce();
         vehilce.run("飞机");
         vehilce.run("小汽车");
-        // 根据单例原则进行修改类
+        // 1.根据交通方式不同分成不同的类
         AirVehicle airVehicle = new AirVehicle();
         airVehicle.run("飞机");
         WaterVehicle waterVehicle = new WaterVehicle();
         waterVehicle.run("轮船");
-        // 根据单例原则修改类中方法
+        // 2.根据交通方式不同分成不同的方法
         VehicleVersion2 vehicleVersion2 = new VehicleVersion2();
         vehicleVersion2.run("小汽车");
         vehicleVersion2.airRun("飞机");
